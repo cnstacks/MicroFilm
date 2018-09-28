7443e208a7ae93591f716a85b2f0a6138732affe
tqtl911@163.com <19930911cXS>
2018-09-28 16:31:59 +0800

    MicroFile Project 的第一个Flask程序！

--- app.py
+++ app.py
@@ -6,5 +6,15 @@
 # File    : app.py
 # Author  : 天晴天朗
 # Email   : tqtl@tqtl.org
+from flask import Flask
+
+app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return "<h1 style = 'color:red'>Hello Flask!</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
