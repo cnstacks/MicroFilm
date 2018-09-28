c3b6a1a8d56001c852d649309832f2930fc409da
tqtl911@163.com <19930911cXS>
2018-09-28 18:23:44 +0800

    创建电影表； 注意datetime.utcnow的用法为： from datetime import datetime

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@
 # Email   : tqtl@tqtl.org
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+from _datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@Tqtl911!@#)^:3306/MicroFilm/"
@@ -21,7 +21,7 @@ class User(db.Model):
     """
     会员表;
     """
-    __tablename__ = "user"
+    __tablename__ = "user"  # 表名
     id = db.Column(db.Integer, primary_key=True)  # 编号
     name = db.Column(db.String(100), unique=True)  # 昵称
     pwd = db.Column(db.String(100), unique=True)  # 密码
@@ -41,7 +41,7 @@ class UserLog(db.Model):
     """
     会员登录日志表
     """
-    __tablename__ = "userlog"
+    __tablename__ = "userlog"  # 表名
     id = db.Column(db.Integer, primary_key=True)  # 编号
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属会员
     ip = db.Column(db.String(100))  # 登录IP
@@ -49,3 +49,40 @@ class UserLog(db.Model):
 
     def __repr__(self):
         return "<UserLog %r>" % self.id
+
+
+class Tag(db.Model):
+    """
+     标签
+    """
+    __tablename__ = "tag"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 标题
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    movies = db.relationship("Movie", backref="tag")  # 电影外键关系的关联
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+
+class Movie(db.Model):
+    """
+    电影表
+    """
+    __tablename__ = "movie"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    url = db.Column(db.String(255), unique=True)  # 地址
+    info = db.Column(db.Text)  # 简介
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    star = db.Column(db.SmallInteger)  # 星级
+    playnum = db.Column(db.BigInteger)  # 播放量
+    commentnum = db.Column(db.BigInteger)  # 评论量
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"))  # 所属标签
+    area = db.Column(db.String(255))  # 上映地区
+    release_time = db.Column(db.Date)  # 上映时间
+    length = db.Column(db.String(100))  # 播放时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Movie %r>" % self.title
