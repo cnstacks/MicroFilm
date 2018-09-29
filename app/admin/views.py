21824306e798cb228e539731148d4a896f748a98
tqtl911@163.com <19930911cXS>
2018-09-29 10:57:41 +0800

    日志管理页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -61,10 +61,6 @@ def preview_list():
     return render_template("admin/preview_list.html")
 
 
-
-
-
-
 @admin.route("/user/list/")
 def user_list():
     return render_template("admin/user_list.html")
@@ -73,3 +69,28 @@ def user_list():
 @admin.route("/user/view/")
 def user_view():
     return render_template("admin/user_view.html")
+
+
+@admin.route("/comment/list/")
+def comment_list():
+    return render_template("admin/comment_list.html")
+
+
+@admin.route("/moviecol/list/")
+def moviecol_list():
+    return render_template("admin/moviecol_list.html")
+
+
+@admin.route("/oplog/list/")
+def oplog_list():
+    return render_template("admin/oplog_list.html")
+
+
+@admin.route("/adminloginlog/list/")
+def adminloginlog_list():
+    return render_template("admin/adminloginlog_list.html")
+
+
+@admin.route("/userloginlog/list/")
+def userloginlog_list():
+    return render_template("admin/userloginlog_list.html")
