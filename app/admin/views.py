67ffcca34e1acec6e860e078a863ce06484bce13
tqtl911@163.com <19930911cXS>
2018-09-29 08:38:22 +0800

    后台登录页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -8,8 +8,22 @@
 # Email   : tqtl@tqtl.org
 
 from . import admin
+from flask import render_template, redirect, url_for
 
 
 @admin.route("/")
 def index():
     return "<h1 style='color:yellow'>This is admin Blueprint<h1>"
+
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.logout"))
+
+
+
